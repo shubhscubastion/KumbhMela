481cd3f83d3377ba00d0f69b76ee25ab91da63c1
Shubh Sarpal <shubh.sarpal@cubastion.com>
2024-08-21 10:43:26 +0530

    Made the component reusable and refactored

--- src/App.js
+++ src/App.js
@@ -6,8 +6,23 @@ function App() {
   return (
     <div>
       <Header />
-
-      <ListofTents price="₹ 1100/-" />
+      <div className="tentContainer">
+        <ListofTents
+          imgsrc="https://tentinkumbh.com/wp-content/uploads/2024/06/Luxury-side-view-1-2048x1152.jpg"
+          tenttype="Basic Tent"
+          price="₹ 1100/-"
+        />
+        <ListofTents
+          imgsrc="https://tentinkumbh.com/wp-content/uploads/2024/06/Premium-room-4-1-2048x1242.jpg"
+          tenttype="Luxury Tent"
+          price="₹ 2500/-"
+        />
+        <ListofTents
+          imgsrc="https://tentinkumbh.com/wp-content/uploads/2024/06/Luxury-inside-view-1-2048x1468.jpg"
+          tenttype="Premium Tent"
+          price="₹ 5000/-"
+        />
+      </div>
     </div>
   );
 }
